588932fc15b31a41ec4a705bf90297d74b28720c
Lakonika <oplokhotnyuk@gmail.com>
2019-06-20 09:46:05 +0900

    Исправляет прелоудер, стили IE11

---
+++ botsad/js/modules/pace.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function() {
+  document.documentElement.classList.remove('no-js');
+  window.paceOptions = {
+    ajax: false,
+    document: false,
+    eventLag: false,
+    elements: false
+  };
+
+})();

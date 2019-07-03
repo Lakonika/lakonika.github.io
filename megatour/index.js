639b098cefb48144c4784dc2e55162def4fdf425
Lakonika <oplokhotnyuk@gmail.com>
2019-07-03 12:55:02 +0900

    Megatour deleted

--- megatour/index.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	'use strict';
-
-	// import swiper from './js/vendor/swiper.min.js'
-	// import slider from './js/modules/slider'
-
-}());
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzIjpbInNyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBpbXBvcnQgc3dpcGVyIGZyb20gJy4vanMvdmVuZG9yL3N3aXBlci5taW4uanMnXG4vLyBpbXBvcnQgc2xpZGVyIGZyb20gJy4vanMvbW9kdWxlcy9zbGlkZXInXG4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0NBQUE7Q0FDQTs7OzsifQ==
